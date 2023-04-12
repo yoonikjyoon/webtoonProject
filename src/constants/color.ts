53840a12e281486a10ddeefb851baad4c1df47cc
Yooni <yooni.kj.yoon@gmail.com>
2023-04-12 13:54:59 +0900

    Add colors constants

---
+++ src/constants/color.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  black: '#000000',
+  white: '#ffffff',
+  gray: '#969696',
+  slateGray: '#3a3a3a',
+  steelGray: '#b7bdc4',
+  coolGray: '#bfbabc',
+  naverGreen: '#64d872',
+  redPink: '#eb564f',
+};
